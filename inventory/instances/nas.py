5eda3f781d3ab6f4feb4249e0d905f8efb964339
Jordan-Cleal_cba <Jordan.Cleal@cba.com.au>
2026-03-08 18:12:28 +1100

    Fix up ansible port and user for NAS

--- inventory/instances/nas.py
+++ inventory/instances/nas.py
@@ -8,6 +8,7 @@ Instance
 from dataclasses import dataclass, field
 
 from .instance import Instance
+from ..env import read_env_var
 
 
 @dataclass
@@ -20,11 +21,15 @@ class NAS(Instance):
         device_name: Network interface name
         name: Instance name template
         host_services: List of host services
+        ansible_port: SSH port for Ansible connections
     """
     name: str = 'jmpa-nas-{id}'
+    ansible_port: int = field(default_factory=lambda: int(read_env_var('NAS_SSH_PORT', default_value='9222')))
 
     def to_dict(self) -> dict:
         """Convert to dictionary with 'nas' key instead of 'instance'."""
         base = self._base_dict()
         base['nas'] = base.pop('instance')
+        base['ansible_port'] = self.ansible_port
+        base['ansible_user'] = base['nas']['name']
         return base
